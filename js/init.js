b9c675ad9630bc6504f84b2a9217ee01e3426c34
carolinagomez1578 <82422286+carolinagomez1578@users.noreply.github.com>
2021-04-12 19:20:14 -0500

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,18 @@
+/*  Inicializa Firebase con la
+ * configuración del proyecto.
+ * Revisa la configuración en tu
+ * servidor de Firebase y cópiala
+ * aquí sustituyendo los
+ * asteriscos. Los campos deben
+ * quedar igual que en tu
+ * servidor. */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyBDFT2jDTXEy9mkAofzY-i3JiCkDM48uNU",
+    authDomain: "bdalum41c.firebaseapp.com",
+    projectId: "bdalum41c",
+    storageBucket: "bdalum41c.appspot.com",
+    messagingSenderId: "1074320306253",
+    appId: "1:1074320306253:web:4c04a42d176c1e4d08ec04",
+    measurementId: "G-55FDQN4TK1"
+});
